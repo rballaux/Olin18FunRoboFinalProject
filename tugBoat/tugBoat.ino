8fa33a2181a0265a2cc82303e5e4dbad05a5870a
rballaux <42899833+rballaux@users.noreply.github.com>
2018-11-30 14:48:54 -0500

    create realtime loop

--- tugBoat/tugBoat.ino
+++ tugBoat/tugBoat.ino
@@ -1,72 +1,189 @@
 // Libraries ------------------------------------------------------------------------------
-#include <Servo.h>; 
-#include <Pixy2.h>; 
-#include <SPI.h>; 
+#include <Servo.h>;
+//#include <Pixy2.h>; //you have to download this separate
+#include <SPI.h>;
 #include <stdio.h>;
+
+// input definitions-------------------------------------------------------------------------
+#define IRPinLeftFront 0
+#define IRPinLeftBack 1
+#define IRPinRightFront 2
+#define IRPinRightBack 3
+
+// Output definitions-------------------------------------------------------------------------
+#define rudderPin 3
+#define propellerPin 4
 
 
 // Initializing Variables ---------------------------------------------------------------------------------
+int rawIRLeftFront;
+int rawIRLeftBack;
+int rawIRRightFront;
+int rawIRRightBack;
+
+const int aliveLED = 13; //create a name for "robot alive" blinky light pin
+const int eStopPin = 12; //create a name for pin connected to ESTOP switch
+boolean aliveLEDState = true; //create a name for alive blinky light state to be used with timer
+boolean ESTOP = true;
+boolean realTimeRunStop = true; //create a name for real time control loop flag
+int command = 0;
+unsigned long oldLoopTime = 0; //create a name for past loop time in milliseconds
+unsigned long newLoopTime = 0; //create a name for new loop time in milliseconds
+unsigned long cycleTime = 0; //create a name for elapsed loop cycle time
+const long controlLoopInterval
+  = 1000 ; //create a name for control loop cycle time in milliseconds
+
+int leftFrontIRDistance = 100;
+int rightFrontIRDistance = 100;
+
 int presenceThreshold = 150; // Threshold that basically says the ir sees something that exists
 int behaviorThreshold = 100; // Threshold that decides whether the boat moves towards or away from something
 int blocks[10];
 
-Servo rudder; Servo throttle; 
+int stateInt = 0;
 
-Pixy2 pixy;
+Servo rudder;
+Servo throttle;
+
+//Pixy2 pixy;
 
 
-void setup() { 
-  Serial.begin(9600);  
-  
-  rudder.attach(1); 
-  throttle.attach(2); 
-  
-  int ir1L = analogRead(1);  
-  int ir1R = analogRead(3);
-  
-  pixy.init();
-} 
+void setup() {
+  pinMode(aliveLED, OUTPUT); // initialize aliveLED pin as an output
+  pinMode(eStopPin, INPUT_PULLUP);
+  Serial.begin(9600);
+  rudder.attach(rudderPin);
+  throttle.attach(propellerPin);
+  //  pixy.init();
+  readAllBoatInput();
+}
 
-void loop(){
-    blocks = pixy.ccc.getBlocks();
-    throttle.write = ; 
-    decideDirectionorIceberg(ir1L, ir2L, ir1R, ir2R)
-} 
+void loop() {
+  //blocks = pixy.ccc.getBlocks();
+  command = getOperatorInput(); // get operator input from serial monitor
+  if (command == "stop") realTimeRunStop = false; // skip real time inner loop
+  else realTimeRunStop = true;
+  readAllBoatInput();
+  //decideDirectionorIceberg(stateInt);
+  while (realTimeRunStop == true) { // if OCU-Stop not commanded, run control loop
+    // Check if operator inputs a command during real-time loop eecution
+    if (Serial.available() > 0) { // check to see if operator typed at OCU
+      realTimeRunStop = false; // if OCU input typed, stop control loop
+      command = Serial.readString().toInt(); // read command string to clear buffer
+      break; // break out of real-time loop
+    }
+    else {
+      realTimeRunStop = true; // if no operator input, run real-time loop
+    }
+    // Real-Time clock control. Check to see if one clock cycle has elapesed before running this controlcode
+    newLoopTime = millis(); // get current Arduino time (50 days till wrap)
+    if (newLoopTime - oldLoopTime >= controlLoopInterval) { // if true run flight code
+      oldLoopTime = newLoopTime; // reset time stamp
+      blinkAliveLED(); // toggle blinky alive light
+      //SENSE-sense---sense---sense---sense---sense---sense---sense---sense---sense---sense---sense-------
+      // TODO add sensor code here
+      // THINK think---think---think---think---think---think---think---think---think---think---think---------
+      // pick robot behavior based on operator input command typed at console
+      if ( command == "stop") {
+        Serial.println("Stop Robot");
+        realTimeRunStop = false; //exit real time control loop
+        break;
+      }
+      else if (command == 1 ) { //Move robot to Operator commanded position
+        Serial.println("Move robot ");
+        //call function to do a certain state
+        Serial.println("Type stop to stop robot");
+        realTimeRunStop = true; //don't exit loop after running once
+      }
+      else if (command == 0) { //Make robot alive with small motions
+        Serial.println("Idle Robot");
+        Serial.println("Type stop to stop robot");
+        realTimeRunStop = true; //run loop continually
+      }
+      else
+      {
+        Serial.println("***** WARNING *******Invalid Input, Robot Stopped, Please try again!");
+        realTimeRunStop = false;
+      }
+      // ACT-act---act---act---act---act---act---act---act---act---act---act---act---act---act------------
+      ESTOP = digitalRead(eStopPin); // check ESTOP switch
+      // Check to see if all code ran successfully in one real-time increment
+      cycleTime = millis() - newLoopTime; // calculate loop execution time
+      if ( cycleTime > controlLoopInterval) {
+        Serial.println("******************************************");
+        Serial.println("error - real time has failed, stop robot!"); // loop took too long to run
+        Serial.print(" 1000 ms real-time loop took = ");
+        Serial.println(cycleTime); // print loop time
+        Serial.println("******************************************");
+        break; // break out of real-time inner loop
+      }
+    } // end of "if (newLoopTime - oldLoopTime >= controlLoopInterval)" real-time loop structure
+  }
+}
 
-void decideDirectionorIceberg() {
-  if (ir1L > presenceThreshold) {// || ir2L > presenceThreshold
-     leftBehavior(); 
-     
-  } else if (ir1R > presenceThreshold){ // || ir2R > presenceThreshold
-     rightBehavior()
+void decideDirectionorIceberg(int stateInt) {
+  if (stateInt == 0) { //this is the circling state
+    if (rawIRLeftFront > presenceThreshold) {// || ir2L > presenceThreshold
+      leftBehavior();
 
-  } else if {pixy.ccc.block > some size {
-     
-  } 
+    } else if (rawIRRightFront > presenceThreshold) { // || ir2R > presenceThreshold
+      rightBehavior();
 
-void leftBehavior(int ir1L) { 
-  if (ir1L > behaviorThreshold){
-     rudder.write(10); // More realistic movement values needed 
-  } else if (ir1L < behaviorThreshold){ 
-    rudderServo.write(-10); 
-  } else {} 
+    }
+  }
 }
 
 
-void rightBehavior(int ir1R) {  // Assuming feeding in the 
-   if (ir1R > behaviorThreshold){ // tolerance + ir2R
-     rudder.write(-10);           //Assuming the rudder turns should be in the opposite direction
-  } else if (ir1R < behaviorThreshold){ // tolerance + ir2R
-    rudder.write = 10; 
+
+void leftBehavior() {
+  if (rawIRLeftFront > behaviorThreshold) {
+    rudder.write(10); // More realistic movement values needed
+  } else if (rawIRLeftFront < behaviorThreshold) {
+    rudder.write(0);
   } else {}
 }
+void rightBehavior() {  // Assuming feeding in the
+  if (rawIRRightFront > behaviorThreshold) { // tolerance + ir2R
+    rudder.write(10);           //Assuming the rudder turns should be in the opposite direction
+  } else if (rawIRRightFront < behaviorThreshold) { // tolerance + ir2R
+    rudder.write(0);
+  } else {}
 }
 
-// This is the pixycam part and I don't know how that works yet
+void readAllBoatInput() {
+  rawIRLeftFront = analogRead(IRPinLeftFront);
+  rawIRLeftBack = analogRead(IRPinLeftBack);
+  rawIRRightFront = analogRead(IRPinRightFront);
+  rawIRRightBack = analogRead(IRPinRightBack);
+}
+int getOperatorInput() {
+  // This function prints operator command options on the serial console and prompts
+  // operator to input desired robot command
+  // Serial.println(" ");
 
-void pixySeeIceberg() { // Need code to make this miss the iceberg by a bit once it gets close
-     if pixy == 
-        throttleServo.write(); 
-        rudderServo.write(pixyDirection...); 
-  
+  Serial.println("==================================================================");
+  Serial.println("| Robot Behavior-Commands: =*int from 0-360* (moves motor to certain degrees), stop(e-stops motors), idle(robot idles)|");
+  Serial.println("| |");
+  //Serial.println("================================================================");
+  Serial.println("| Please type desired robot behavior in command line at top of this window |");
+  Serial.println("| and then press SEND button. |");
+  Serial.println("==================================================================");
+  while (Serial.available() == 0) {}; // do nothing until operator input typed
+  command = Serial.readString().toInt(); // read command string
+  Serial.print("| New robot behavior command is: "); // give command feedback to operator
+  Serial.println(command);
+  Serial.println("| Type 'stop' to stop control loop and wait for new command |");
+  Serial.println("==================================================================");
+  return command;
+}
+void blinkAliveLED() {
+  // This function toggles state of aliveLED blinky light LED
+  // if the LED is off turn it on and vice-versa:
+  if (aliveLEDState == LOW) {
+    aliveLEDState = HIGH;
+  } else {
+    aliveLEDState = LOW;
+  }
+  // set the LED with the ledState of the variable:
+  digitalWrite(aliveLED, aliveLEDState);
 }
